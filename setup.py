0d4e8825056a27ccd2f41b03f288e416a13d191c
thovo <votuongtho@gmail.com>
2014-12-02 17:21:07 +0100

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,29 @@
-from distutils.core import setup
+__author__ = 'thovo'
+import os
+
+from setuptools import setup, find_packages
 
 setup(
-    name='aa_practice_project',
-    version='1',
-    packages=['', 'algorithms', 'XMLParsingScript'],
+    name='TSP',
+    version='0.0.1',
+    description='Implement the algorithms for TSP problem',
+    packages=find_packages(exclude='test_scripts'),
+    install_requires=['xlwt','prettytable','numpy'],
     url='https://github.com/thovo/aa_practice_project',
     license='GNU General Public License (GPL)',
     author='Tho VO,Ahmed Hassan,Omar Samir',
     author_email='Tho VO<votuongtho@gmail.com>;Omar Samir <omar.samir3000@gmail.com>;Ahmed Hassan <ahmed.adel.hassan@hotmail.com>',
-    description='Use for our practice project in TSP problems'
+    long_description='Use for our practice project in TSP problems',
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 1 - Conceptual',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: GPL License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
